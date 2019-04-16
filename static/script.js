e0ba54f139c1cefa34e8e15598e3581ed079ea96
Momozor <skelic3@gmail.com>
2019-04-16 10:52:58 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,19 @@
+//  This file is part of Arpachat.
+//  Copyright (C) 2019  Momozor
+
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 $(function() {
     const HOST = 'localhost' // change this to your preferred domain
     const PORT = 3000 // you can change this port if you wish
